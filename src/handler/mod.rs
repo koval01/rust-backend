78695e5a13537c7508c47b7f7f73dd888132e07c
Koval Yaroslav <yaroslav@koval.page>
2025-01-08 13:06:44 +0100

    Remove unused handlers from handler/mod

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,10 +1,9 @@
 mod health;
 mod user;
+mod lesson;
 
 pub use health::health_checker_handler;
 pub use user::{
-    user_handler_get, 
-    user_handler_post, 
-    user_handler_put, 
+    user_handler_get,
     user_id_handler_get
 };
