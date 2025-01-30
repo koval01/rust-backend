b52f801ea3b67ea5b5b0ec6fef12dcbdbc91d53a
Koval Yaroslav <yaroslav@koval.page>
2025-01-30 17:02:49 +0100

    Temporary version of handlers

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,10 +1,12 @@
 mod health;
-mod user;
-mod lesson;
+mod auth;
+// mod user;
+// mod lesson;
 
 pub use health::health_checker_handler;
-pub use user::{
-    user_handler_get,
-    user_id_handler_get
-};
-pub use lesson::{lesson_handler_get};
+pub use auth::{login, callback};
+// pub use user::{
+//     user_handler_get,
+//     user_id_handler_get
+// };
+// pub use lesson::{lesson_handler_get};
