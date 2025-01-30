64c62efe9683899b3cd2cb497e56e1d80b53dc63
Koval Yaroslav <yaroslav@koval.page>
2025-01-30 17:02:13 +0100

    Temporary version of middleware

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,9 +1,9 @@
 mod validate;
 mod request_id;
 mod timestamp_guard;
-mod sync_user;
+// mod sync_user;
 
 pub use request_id::request_id_middleware;
-pub use sync_user::sync_user_middleware;
+// pub use sync_user::sync_user_middleware;
 pub use timestamp_guard::timestamp_guard_middleware;
 pub use validate::validate_middleware;
