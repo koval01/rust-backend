bfa15b216590c72dff38fae897c4d59e29030f8f
Koval Yaroslav <yaroslav@koval.page>
2025-01-30 17:01:24 +0100

    Update User model

--- src/model/user.rs
+++ src/model/user.rs
@@ -1,12 +1,33 @@
+use std::collections::BTreeMap;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct User {
-    pub id: i64,
-    pub first_name: String,
-    pub last_name: Option<String>,
-    pub username: Option<String>,
-    pub language_code: String,
-    pub allows_write_to_pm: bool,
-    pub photo_url: Option<String>,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct GoogleUser {
+    pub id: String,
+    pub email: String,
+    pub verified_email: bool,
+    pub name: String,
+    pub given_name: String,
+    pub family_name: Option<String>,
+    pub picture: Option<String>,
+}
+
+impl GoogleUser {
+    pub fn to_btree_map(&self) -> BTreeMap<&str, &str> {
+        let mut map = BTreeMap::new();
+
+        map.insert("id", self.id.as_str());
+        map.insert("email", self.email.as_str());
+        map.insert("name", self.name.as_str());
+        map.insert("given_name", self.given_name.as_str());
+
+        if let Some(ref family_name) = self.family_name {
+            map.insert("family_name", family_name.as_str());
+        }
+        if let Some(ref picture) = self.picture {
+            map.insert("picture", picture.as_str());
+        }
+
+        map
+    }
 }
