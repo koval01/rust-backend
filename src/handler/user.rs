7be5d790df85b10fa52d04b6148a4814f1c6fa39
Koval Yaroslav <yaroslav@koval.page>
2025-01-31 20:38:11 +0100

    Update user handlers

--- src/handler/user.rs
+++ src/handler/user.rs
@@ -10,7 +10,7 @@ use crate::{
     error::ApiError,
     model::GoogleUser,
     prisma::*,
-    response::{ApiResponse, UserResponseData},
+    response::ApiResponse,
     util::cache::CacheWrapper,
     Extension,
     cache_db_query
@@ -21,12 +21,21 @@ use moka::future::Cache;
 type Database = Extension<Arc<PrismaClient>>;
 
 macro_rules! get_user {
+    ($cache:expr, $db:expr, $id:expr, @google_id) => {
+        get_user_internal!($cache, $db, $id, user::google_id::equals($id))
+    };
     ($cache:expr, $db:expr, $id:expr) => {
+        get_user_internal!($cache, $db, $id, user::id::equals($id))
+    };
+}
+
+macro_rules! get_user_internal {
+    ($cache:expr, $db:expr, $id:expr, $condition:expr) => {
         cache_db_query!(
             $cache,
             &format!("user:{}", $id),
             $db.user()
-                .find_first(vec![user::google_id::equals($id)])
+                .find_first(vec![$condition])
                 .exec()
                 .await,
             |_| ApiError::NotFound("User does not exist".to_string())
@@ -45,7 +54,7 @@ pub async fn user_handler_get(
     let cache = CacheWrapper::<user::Data>::new(redis_pool, moka_cache, 600);
 
     // Attempt to fetch the user from cache or database
-    let user = get_user!(cache, db, user_id)?;
+    let user = get_user!(cache, db, user_id, @google_id)?;
 
     let response = ApiResponse::success(user);
     Ok((StatusCode::OK, Json(response)))
@@ -54,7 +63,7 @@ pub async fn user_handler_get(
 /// Handles GET requests for a user by ID
 pub async fn user_id_handler_get(
     _: GoogleUser,
-    Path(id): Path<i64>,
+    Path(id): Path<uuid::Uuid>,
     Extension(redis_pool): Extension<Pool<RedisConnectionManager>>,
     Extension(moka_cache): Extension<Cache<String, String>>,
     db: Database
